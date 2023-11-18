9745fd2ec0224366ce45c2c9ee879da8a6cd0b69
Yoman666 <ivy410384@gmail.com>
2023-11-18 16:25:36 +0800

    donatenumber

--- js/myScript.js
+++ js/myScript.js
@@ -27,22 +27,21 @@ document.addEventListener('click', function(event) {
   function donationSuccess() {
     alert('捐款成功！');
   }
+  
   // ----------
   
   function submitForm(event) {
-    event.preventDefault(); // 防止表单默认提交行为
+    event.preventDefault();
   
-    // 获取表单字段的值
     var nameValue = document.getElementById('nameInput').value;
     var emailValue = document.getElementById('emailInput').value;
     var commentValue = document.getElementById('commentInput').value;
   
-    // 检查是否所有字段都已填写
+    // 檢查
     if (nameValue && emailValue && commentValue) {
-      // 显示 Bootstrap Modal
       $('#successModal').modal('show');
   
-      // 清空表单字段
+      // 清空表單
       document.getElementById('nameInput').value = '';
       document.getElementById('emailInput').value = '';
       document.getElementById('commentInput').value = '';
@@ -53,44 +52,43 @@ document.addEventListener('click', function(event) {
 
 
 //   mose 
-// $(window).mousemove(function (evt) {
-//     var x = evt.pageX;
-//     var y = evt.pageY;
-//     console.log(x);
+$(window).mousemove(function (evt) {
+    var x = evt.pageX;
+    var y = evt.pageY;
+    console.log(x);
   
-//     $("#cross").css("left", x + "px");
-//     $("#cross").css("top", y + "px");
+    $("#cross").css("left", x + "px");
+    $("#cross").css("top", y + "px");
   
-//     if (x < $(window).width() / 2 - 50) {
-//       $("#cat").attr("src", "https://awiclass.monoame.com/catpic/cat_left.png");
-//     } else if (x > $(window).width() / 2 + 50) {
-//       $("#cat").attr("src", "https://awiclass.monoame.com/catpic/cat_right.png");
-//     } else {
-//       $("#cat").attr("src", "https://awiclass.monoame.com/catpic/cat_top.png");
-//     }
-//     if (x < $(window).width() / 2 - 50 && y < $(window).height() / 2) {
-//       $("#cat").attr(
-//         "src",
-//         "https://awiclass.monoame.com/catpic/cat_lefttop.png"
-//       );
-//     }
-//     if (x > $(window).width() / 2 + 50 && y < $(window).height() / 2) {
-//       $("#cat").attr(
-//         "src",
-//         "https://awiclass.monoame.com/catpic/cat_righttop.png"
-//       );
-//     }
-//   });
+    if (x < $(window).width() / 2 - 50) {
+      $("#cat").attr("src", "https://awiclass.monoame.com/catpic/cat_left.png");
+    } else if (x > $(window).width() / 2 + 50) {
+      $("#cat").attr("src", "https://awiclass.monoame.com/catpic/cat_right.png");
+    } else {
+      $("#cat").attr("src", "https://awiclass.monoame.com/catpic/cat_top.png");
+    }
+    if (x < $(window).width() / 2 - 50 && y < $(window).height() / 2) {
+      $("#cat").attr(
+        "src",
+        "https://awiclass.monoame.com/catpic/cat_lefttop.png"
+      );
+    }
+    if (x > $(window).width() / 2 + 50 && y < $(window).height() / 2) {
+      $("#cat").attr(
+        "src",
+        "https://awiclass.monoame.com/catpic/cat_righttop.png"
+      );
+    }
+  });
   
   
   // donationnum
   document.addEventListener('click', function(event) {
-    // 检查点击的元素是否是捐款选项
     if (!event.target.closest('.singledonate')) {
       selectDonationId = '';
     }
   });
-  
+
   var selectDonationId="";
   function slectId (id){
     selectDonationId = id;
@@ -101,14 +99,44 @@ document.addEventListener('click', function(event) {
     if(!selectDonationId){
       alert("請選擇一項");
       return;
+    }else if(selectDonationId == 'num04'){
+      var singleDonateNum = document.getElementById(selectDonationId).querySelector('.dimput');
+      console.log(singleDonateNum);
+      var singleIntNum = parseInt(singleDonateNum.value);
+      var totalNum = document.getElementById('totalnum');
+      var totalIntNum = parseInt(totalNum.textContent.replace(/,/g, ''), 10);
+      var addNum = parseInt(totalIntNum + singleIntNum);
+
+      var peoplenum = document.getElementById(selectDonationId).querySelector('.dnum');
+      var intpeoplenum = parseInt(peoplenum.textContent) + 1;
+      peoplenum.textContent = intpeoplenum ;
+
+      totalNum.textContent = addNum.toLocaleString();
+      alert('捐款成功!');
+
+      
     }
-    var singleDonateNum = document.getElementById(selectDonationId).querySelector('.dmoney');
-    var singleIntNum = parseInt(singleDonateNum.textContent.replace('NT$','').replace('',''));
-    var totalNum = document.getElementById('totalnum');
-    var totalIntNum = parseInt(totalNum.textContent.replace(/,/g, ''), 10);
-    var addNum = parseInt(totalIntNum + singleIntNum);
-    totalNum.textContent = addNum.toLocaleString(); // 使用 toLocaleString 添加千位分隔符
-    console.log('new', totalNum.textContent);
-    alert('捐款成功!');
+    else{
+      var singleDonateNum = document.getElementById(selectDonationId).querySelector('.dmoney');
+      var singleIntNum = parseInt(singleDonateNum.textContent.replace('NT$','').replace('',''));
+      var totalNum = document.getElementById('totalnum');
+      var totalIntNum = parseInt(totalNum.textContent.replace(/,/g, ''), 10);
+      var addNum = parseInt(totalIntNum + singleIntNum);
+      totalNum.textContent = addNum.toLocaleString(); // 使用 toLocaleString 添加千位分隔符
+
+      var peoplenum = document.getElementById(selectDonationId).querySelector('.dnum');
+      var intpeoplenum = parseInt(peoplenum.textContent) + 1;
+      peoplenum.textContent = intpeoplenum ;
+
+      console.log(intpeoplenum);
+      alert('捐款成功!');
+    }
     // $('#customModal').modal('show');
   }
+
+  
+ 
+  
+
+
+
